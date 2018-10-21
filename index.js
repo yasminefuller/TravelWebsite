97ed004cde86642d1056cda393c9c731f62def92
yasminefuller <fulleryaz@gmail.com>
2018-10-20 22:16:30 -0400

    Created .hide & .shows for each header's page

--- index.js
+++ index.js
@@ -1,12 +1,48 @@
 $(document).ready(() => {
-    const $about = $(".about");
-    const $aboutPage = $("#about-page");
 
-    $aboutPage.hide();
+    $("#about-page").hide();
+    $("#tips-tricks-page").hide();
+    $("#trips-page").hide();
+    $("#contact-page").hide();
 
-    $about.on('click', () => {
-        $aboutPage.show();
-        $(".home").hide();
-
+    $(".about").on('click', () => {
+        $("#about-page").show();
+        $("#home-page").hide();
+        $("#tips-tricks-page").hide();
+        $("#trips-page").hide();
+        $("#contact-page").hide();
     });
+
+    $(".tips-tricks").on('click', () => {
+        $("#tips-tricks-page").show();
+        $("#home-page").hide();
+        $("#about-page").hide();
+        $("#trips-page").hide();
+        $("#contact-page").hide();
+    });
+
+    $(".trips").on('click', () => {
+        $("#trips-page").show();
+        $("#home-page").hide();
+        $("#tips-tricks-page").hide();
+        $("#about-page").hide();
+        $("#contact-page").hide();
+    });
+
+    $(".contact").on('click', () => {
+        $("#contact-page").show();
+        $("#home-page").hide();
+        $("#tips-tricks-page").hide();
+        $("#about-page").hide();
+        $("#trips-page").hide();
+    });
+
+    $(".home").on('click', () => {
+        $("#home-page").show();
+        $("#contact-page").hide();
+        $("#tips-tricks-page").hide();
+        $("#about-page").hide();
+        $("#trips-page").hide();
+    });
+
 });
