5ffe25b8ffd65b91a132a6bd41462a4d79c8c24d
yasminefuller <fulleryaz@gmail.com>
2018-10-23 17:58:51 -0400

    10.23.18 Updates - Images, About page copy, some of home page copy, contact info.

--- index.js
+++ index.js
@@ -1,48 +1,101 @@
 $(document).ready(() => {
 
-    $("#about-page").hide();
-    $("#tips-tricks-page").hide();
-    $("#trips-page").hide();
-    $("#contact-page").hide();
+    $('#about-page').hide();
+    $('#tips-tricks-page').hide();
+    $('#trips-page').hide();
+    $('#contact-page').hide();
+    $('.nav-trips').hide();
 
-    $(".about").on('click', () => {
-        $("#about-page").show();
-        $("#home-page").hide();
-        $("#tips-tricks-page").hide();
-        $("#trips-page").hide();
-        $("#contact-page").hide();
+    $('.about').on('click', () => {
+        $('#about-page').show();
+        $('#home-page').hide();
+        $('#tips-tricks-page').hide();
+        $('#trips-page').hide();
+        $('#contact-page').hide();
+    }).on('mouseenter', () => {
+        $('.about').css({
+            color: 'blue',
+            backgroundColor: 'grey'
+        });
+    }).on('mouseleave', () => {
+        $('.about').css({
+            color: 'black',
+            backgroundColor: 'beige'
+        });
     });
 
-    $(".tips-tricks").on('click', () => {
-        $("#tips-tricks-page").show();
-        $("#home-page").hide();
-        $("#about-page").hide();
-        $("#trips-page").hide();
-        $("#contact-page").hide();
+    $('.tips-tricks').on('click', () => {
+        $('#tips-tricks-page').show();
+        $('#home-page').hide();
+        $('#about-page').hide();
+        $('#trips-page').hide();
+        $('#contact-page').hide();
+    }).on('mouseenter', () => {
+        $('.tips-tricks').css({
+            color: 'blue',
+            backgroundColor: 'grey'
+        });
+    }).on('mouseleave', () => {
+        $('.tips-tricks').css({
+            color: 'black',
+            backgroundColor: 'beige'
+        });
     });
 
-    $(".trips").on('click', () => {
-        $("#trips-page").show();
-        $("#home-page").hide();
-        $("#tips-tricks-page").hide();
-        $("#about-page").hide();
-        $("#contact-page").hide();
+    $('.trips').on('click', () => {
+        $('#trips-page').show();
+        $('#home-page').hide();
+        $('#tips-tricks-page').hide();
+        $('#about-page').hide();
+        $('#contact-page').hide();
+    }).on('mouseenter', () => {
+//      $('.nav-trips').show(); Creates dropdown
+        $('.trips').css({
+            color: 'blue',
+            backgroundColor: 'grey'
+        });
+    }).on('mouseleave', () => {
+//      $('.nav-trips').hide(); Creates dropdown
+        $('.trips').css({
+            color: 'black',
+            backgroundColor: 'beige'
+        });
     });
 
-    $(".contact").on('click', () => {
-        $("#contact-page").show();
-        $("#home-page").hide();
-        $("#tips-tricks-page").hide();
-        $("#about-page").hide();
-        $("#trips-page").hide();
+    $('.contact').on('click', () => {
+        $('#contact-page').show();
+        $('#home-page').hide();
+        $('#tips-tricks-page').hide();
+        $('#about-page').hide();
+        $('#trips-page').hide();
+    }).on('mouseenter', () => {
+        $('.contact').css({
+            color: 'blue',
+            backgroundColor: 'grey'
+        });
+    }).on('mouseleave', () => {
+        $('.contact').css({
+            color: 'black',
+            backgroundColor: 'beige'
+        });
     });
 
-    $(".home").on('click', () => {
-        $("#home-page").show();
-        $("#contact-page").hide();
-        $("#tips-tricks-page").hide();
-        $("#about-page").hide();
-        $("#trips-page").hide();
+    $('.home').on('click', () => {
+        $('#home-page').show();
+        $('#contact-page').hide();
+        $('#tips-tricks-page').hide();
+        $('#about-page').hide();
+        $('#trips-page').hide();
+    }).on('mouseenter', () => {
+        $('.home').css({
+            color: 'blue',
+            backgroundColor: 'grey'
+        });
+    }).on('mouseleave', () => {
+        $('.home').css({
+            color: 'black',
+            backgroundColor: 'beige'
+        });
     });
 
 });
